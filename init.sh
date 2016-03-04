9e6244f4382c6b3bc96af753ec88b802af40b2fe
Nikolay Markin <nn.markin>
2016-03-04 12:50:05 +0300

    Добавлено решение второй задачи
    Signed-off-by: Nikolay Markin <nn.markin>

--- init.sh
+++ init.sh
@@ -2,3 +2,6 @@ sudo rm /etc/nginx/sites-enabled/test.conf
 sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo rm /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
+
+sudo ln -s /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
+sudo /etc/init.d/gunicorn restart
---
+++ web/hello.py
@@ -0,0 +1,18 @@
+def app(environ, start_response):
+
+	
+	raw_uri = str(environ.get('RAW_URI'))
+	
+	raw_uri = raw_uri[2:]
+
+	params = raw_uri.split('&')
+	
+	data = ''
+	for param in params:
+		data += param + '\r\n'
+	
+	start_response("200 OK", [
+	  ("Content-Type", "text/plain"),
+	  ("Content-Length", str(len(data)))
+	])
+	return iter([data])
