ce5d3807910fb7079b4c76e9f3bd655622bc92c2
Nikolay Markin <nn.markin>
2016-03-15 17:18:02 +0300

    213

--- web/ask/qa/models.py
+++ web/ask/qa/models.py
@@ -21,7 +21,7 @@ class QuestionManager(models.Manager):
 class Question(models.Model):
 	title = models.CharField(max_length=255)
 	text = models.TextField()
-	added_at = models.DateTimeField()
+	added_at = models.DateTimeField(auto_now_add=True)
 	rating = models.IntegerField()
 	author = models.ForeignKey(User, related_name='question_user')
 	likes = models.ManyToManyField(User, related_name='%(app_label)s_%(class)s_related', blank=True)
@@ -35,6 +35,6 @@ class Question(models.Model):
 # author - автор ответа
 class Answer(models.Model):
 	text = models.TextField()
-	added_at = models.DateTimeField()
+	added_at = models.DateTimeField(auto_now_add=True)
 	question =  models.ForeignKey(Question)
 	author = models.ForeignKey(User)
