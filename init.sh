671ab786dfc221c375d0f432dbea56d46ba4a351
Nikolay Markin <markoni.nik@gmail.com>
2016-03-15 19:44:44 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -6,4 +6,6 @@ sudo /etc/init.d/nginx restart
 sudo ln -s /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
 sudo /etc/init.d/gunicorn restart
 
+sudo /etc/init.d/mysql restart
 mysql -u root -e "CREATE DATABASE qa CHARACTER SET utf8"
+
