613f821e1cd259c4b7053bb435bc71f23c0de62d
MELXIORY <83028802+melxiory@users.noreply.github.com>
2022-11-10 19:29:44 +0300

    Update settings.py

--- web/ask/ask/settings.py
+++ web/ask/ask/settings.py
@@ -52,7 +52,7 @@ MIDDLEWARE_CLASSES = (
 
 ROOT_URLCONF = 'ask.urls'
 
-TEMPLATE_DIRS = (os.path.join(BASE_DIR, 'qa/templates'))
+TEMPLATE_DIRS = ('~/web/ask/qa/templates')
 
 WSGI_APPLICATION = 'ask.wsgi.application'
 
